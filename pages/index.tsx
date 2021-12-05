6446f2ceb5316c1ff2ee796dc336826819ca22aa
kingbri <bdashore3@gmail.com>
2021-12-04 22:04:19 -0500

    Initial commit
    Signed-off-by: kingbri <bdashore3@gmail.com>

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      Hello, this is a nextjs site
+    </div>
+  )
+}
